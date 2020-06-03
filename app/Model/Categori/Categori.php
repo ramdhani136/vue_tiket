4fb38185da24468fc4dbbb3c0720904339993f1c
Ilham Ramdhani <ramdhaniit@gmail.com>
2020-06-03 22:22:16 +0700

    table categori

---
+++ app/Model/Categori/Categori.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Categori;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categori extends Model
+{
+    protected $guarded=[];
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Model\Categori\Categori;
 
 class DatabaseSeeder extends Seeder
+
+
+
 {
     /**
      * Seed the application's database.
@@ -12,5 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(User::class,20)->create();
+        factory(Categori::class,20)->create();
     }
 }
