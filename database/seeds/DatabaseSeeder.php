626a4ed5c708ae85a8ddfd3eed9aae4b55e9e910
Ilham Ramdhani <ramdhaniit@gmail.com>
2020-06-03 22:53:19 +0700

    table transaksi

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use App\User;
 use App\Model\Categori\Categori;
 use App\Model\Tiket\Tiket;
+use App\Model\Transaksi\Transaksi;
 
 class DatabaseSeeder extends Seeder
 
@@ -17,9 +18,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+         //$this->call(UserSeeder::class);
         //factory(User::class,20)->create();
-        ///factory(Categori::class,20)->create();
-        factory(Tiket::class,20)->create();
+        //factory(Categori::class,20)->create();
+        //factory(Tiket::class,20)->create();
+        factory(Transaksi::class,20)->create();
     }
 }
