e8b745208f4760aca05af32c814dcfa71e2387d5
Ilham Ramdhani <ramdhaniit@gmail.com>
2020-06-03 22:39:14 +0700

    table tiket

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\User;
 use App\Model\Categori\Categori;
+use App\Model\Tiket\Tiket;
 
 class DatabaseSeeder extends Seeder
 
@@ -17,7 +18,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        factory(User::class,20)->create();
-        factory(Categori::class,20)->create();
+        //factory(User::class,20)->create();
+        ///factory(Categori::class,20)->create();
+        factory(Tiket::class,20)->create();
     }
 }
